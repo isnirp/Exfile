b77e95e8036b11390c65aa8705f196f48621cf9e
isnirp <pramankwah@gmail.com>
2020-04-29 09:55:08 +0200

    breadcrumb onclick listener

--- app/src/main/java/com/flimbis/exfile/view/adapter/BreadcrumbAdapter.kt
+++ app/src/main/java/com/flimbis/exfile/view/adapter/BreadcrumbAdapter.kt
@@ -1,29 +1,20 @@
 package com.flimbis.exfile.view.adapter
 
-import androidx.databinding.DataBindingUtil
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import com.flimbis.exfile.R
 import com.flimbis.exfile.ctx
-import com.flimbis.exfile.databinding.ItemsFileBinding
 import com.flimbis.exfile.model.FileModel
-import com.flimbis.exfile.viewmodel.FileViewModel
 
-
-/*
-* DEPRECATED
-* */
-//class BreadcrumbAdapter(val items: List<FileModel>, val itemClick: (FileModel)-> Unit) : androidx.recyclerview.widget.RecyclerView.Adapter<BreadcrumbAdapter.FileViewHolder>() {
 class BreadcrumbAdapter() : androidx.recyclerview.widget.RecyclerView.Adapter<BreadcrumbAdapter.FileViewHolder>() {
 
     private var items = listOf<FileModel>()
     var onItemClickListener: ((FileModel) -> Unit)? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FileViewHolder {
-        val layoutInflater = LayoutInflater.from(parent.ctx)
-        val view: View = layoutInflater.inflate(R.layout.items_breadcrumb, parent, false)
+        val view: View = LayoutInflater.from(parent.ctx).inflate(R.layout.items_breadcrumb, parent, false)
 
         return FileViewHolder(view)
     }
@@ -31,11 +22,7 @@ class BreadcrumbAdapter() : androidx.recyclerview.widget.RecyclerView.Adapter<Br
     override fun getItemCount(): Int = items.size
 
     override fun onBindViewHolder(holder: FileViewHolder, position: Int) {
-        val fileModel = items[position]
-
-        holder.bind(fileModel)
-        //holder.binding.root.setOnClickListener{listener!!.onBreadcrumbItemClicked(fileModel)}
-
+        holder.bind(items[position])
     }
 
     fun updateBreadcrumbList(filesList: List<FileModel>) {
@@ -43,18 +30,19 @@ class BreadcrumbAdapter() : androidx.recyclerview.widget.RecyclerView.Adapter<Br
         notifyDataSetChanged()
     }
 
-    //class FileViewHolder(var binding: ItemsFileBinding, val itemClick: (FileModel)-> Unit) : androidx.recyclerview.widget.RecyclerView.ViewHolder(binding.lnrLayout){
     inner class FileViewHolder(itemView: View) : androidx.recyclerview.widget.RecyclerView.ViewHolder(itemView), View.OnClickListener {
+        init {
+            itemView.setOnClickListener(this)
+        }
+
         fun bind(fileModel: FileModel) {
             val path = itemView.findViewById<TextView>(R.id.txt_breadcrumb)
             if (fileModel.name == "exHome") path.text = "/"
             else path.text = fileModel.name
-
         }
 
         override fun onClick(v: View?) {
             onItemClickListener?.invoke(items[adapterPosition])
         }
     }
-
 }
