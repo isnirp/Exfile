cd9f5a6d3def7ee41d617bae538d82c35e0dc816
isnirp <pramankwah@gmail.com>
2020-05-06 17:14:00 +0200

    navigate up stack

--- app/src/main/java/com/flimbis/exfile/view/settings/SettingsActivity.kt
+++ app/src/main/java/com/flimbis/exfile/view/settings/SettingsActivity.kt
@@ -1,6 +1,9 @@
 package com.flimbis.exfile.view.settings
 
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
+import androidx.core.app.NavUtils
 import com.flimbis.exfile.BaseActivity
 import com.flimbis.exfile.R
 
@@ -9,9 +12,25 @@ class SettingsActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar_settings)
+        setSupportActionBar(toolbar)
+        title = "Settings"
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
         supportFragmentManager
                 .beginTransaction()
                 .replace(R.id.content_settings, SettingsFragment())
                 .commit()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when (item?.itemId) {
+            android.R.id.home -> {
+                NavUtils.navigateUpFromSameTask(this)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
